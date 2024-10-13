e29d855d27543405c834727566e347d08640cb69
Vicente Olivares <vicente.olivares@ug.uchile.cl>
2024-10-13 01:59:35 -0300

    feat: add whisper voice recognition

--- VoiceRecognition.py
+++ VoiceRecognition.py
@@ -3,12 +3,22 @@ import wave
 from vosk import Model, KaldiRecognizer
 from constants import VOICE_MODEL_PATH
 import json
+from openai import OpenAI
 
 class VoiceRecognition:
+    """
+    Class to transcribe audio files to text
+    """
+    def to_text(self, wavefile: str | UploadedFile, client: OpenAI):
+        """
+        Transcribe the audio file to text
+        """
+
+        return self.whisper_to_text(wavefile, client)
 
     def vosk_to_text(self, wavefile: str | UploadedFile):
         """
-        Transcribe the audio file to text
+        Transcribe the audio file to text using Vosk
         """
 
         model = Model(VOICE_MODEL_PATH)
@@ -23,3 +33,14 @@ class VoiceRecognition:
                     pass
             result = json.loads(rec.FinalResult())
             return result["text"]
+        
+
+    def whisper_to_text(self, wavefile: str | UploadedFile, client: OpenAI):
+        """
+        Transcribe the audio file to text using OpenAI
+        """
+        transcription = client.audio.transcriptions.create(
+            file=wavefile,
+            model="whisper-1"
+        )
+        return transcription.text
