cc99e17f3efca8f712ecde188f3627b378820609
Lucas-CE <lucas.carrascoestay@gmail.com>
2024-10-13 04:19:01 -0300

    Refactor imports

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ from constants import PROMPTS_PATH, COURSES_INFO_PATH
 import json
 import os
 from VoiceRecognition import VoiceRecognition
+from dotenv import load_dotenv
 
 st.set_page_config(
     page_title="Womby",
