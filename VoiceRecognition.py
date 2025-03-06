ae63b701a52f63f2b7c94f8c86dfeb1ce11629a0
Vicente Olivares Gómez <100884725+Vicenturro14@users.noreply.github.com>
2025-03-06 19:46:36 -0300

    Delete vosk model from VoiceRecognition class

--- VoiceRecognition.py
+++ VoiceRecognition.py
@@ -1,7 +1,5 @@
 from streamlit.runtime.uploaded_file_manager import UploadedFile
 import wave
-from vosk import Model, KaldiRecognizer
-from constants import VOICE_MODEL_PATH
 import json
 from openai import OpenAI
 
@@ -15,24 +13,6 @@ class VoiceRecognition:
         """
 
         return self.whisper_to_text(wavefile, client)
-
-    def vosk_to_text(self, wavefile: str | UploadedFile):
-        """
-        Transcribe the audio file to text using Vosk
-        """
-
-        model = Model(VOICE_MODEL_PATH)
-
-        with wave.open(wavefile, "rb") as wf:
-            rec = KaldiRecognizer(model, wf.getframerate())
-            while True:
-                data = wf.readframes(4000)
-                if len(data) == 0:
-                    break
-                if rec.AcceptWaveform(data):
-                    pass
-            result = json.loads(rec.FinalResult())
-            return result["text"]
         
 
     def whisper_to_text(self, wavefile: str | UploadedFile, client: OpenAI):
